2e6e2c65af10a9da00cd37e1ea19f06ae0f54060
mkg20001 <mkg20001@gmail.com>
2017-08-12 18:25:21 +0200

    Common module

---
+++ common.js
@@ -0,0 +1,29 @@
+"use strict"
+
+function consolePrefix(f, p) {
+  return function () {
+    const a = [...arguments]
+    if (typeof a[0] == "string") a.unshift("[" + p + "] " + a[0])
+    else a.unshift("[" + p + "]")
+    f.apply(console, a)
+  }
+}
+
+module.exports = function ZeroNetBrowserCommon(opt) {
+  const self = this
+  self.browser = true
+  self.logger = name => {
+    const l = consolePrefix(console.log, name)
+    l.info = l
+    l.log = l
+
+    l.debug = opt.debug ? consolePrefix(console.info, "DEBUG/" + name) : () => {}
+    l.trace = opt.debug ? consolePrefix(console.info, "TRACE/" + name) : () => {}
+
+    l.warn = consolePrefix(console.warn, name)
+
+    l.error = l.fatal = consolePrefix(console.error, name)
+    
+    return l
+  }
+}
